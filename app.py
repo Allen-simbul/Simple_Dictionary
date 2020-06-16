d7a8ccbc54cba3967d730ffddd2f41a12e52c7c4
allen <allen.simbul591@gmail.com>
2020-06-16 18:39:44 +0100

    added support for multiple checks depending on user input

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 data = json.load(open('data.json'))
 
 
@@ -7,10 +8,20 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        confirm = input(
+            f'Did you mean {get_close_matches(word, data.keys())[0]} instead? Enter Y for yes, or N for no > ')
+        confirm = confirm.lower()
+        if confirm == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif confirm == 'n':
+            return 'The word doesnt exist, please double check it!'
+        else:
+            return 'We didnt understand your entry. Please try again'
     else:
         return 'The word doesnt exist, please double check it!'
 
 
-word = input('Enter Word: ')
+word = input('Enter Word > ')
 
 print(translate(word))
