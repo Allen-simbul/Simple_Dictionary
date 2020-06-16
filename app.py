186518592d245390f4ee68a5752be4fcf488daf2
allen <allen.simbul591@gmail.com>
2020-06-16 17:01:40 +0100

    initial commit to project

---
+++ app.py
@@ -0,0 +1,15 @@
+import json
+data = json.load(open('data.json'))
+
+
+def translate(word):
+
+    if word in data:
+        return data[word]
+    else:
+        return 'The word doesnt exist, please double check it!'
+
+
+word = input('Enter Word: ')
+
+print(translate(word))
